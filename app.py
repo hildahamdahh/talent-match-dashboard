fd97e20d47f88d84b8751764e1ca75e6792ee461
Hilda Hamdah Husniyyah <114492834+hildahamdahh@users.noreply.github.com>
2025-10-29 23:52:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ Original file is located at
 import streamlit as st
 from supabase import create_client, Client
 import pandas as pd
+import plotly.express as px
 
 # ==========================================================
 # 🔗 KONEKSI SUPABASE
